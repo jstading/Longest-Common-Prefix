cba13e09cbc9e2383cc03520da75d08ed9381dcc
John Stading <jstading@gmail.com>
2023-03-06 21:00:22 -0600

    Initial Version

---
+++ Longest-Common-Prefix.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/longest-common-prefix/
+
+class Solution(object):
+    def largest(self, strs):
+        print(strs)
+
+
+"""Test Cases
+Input: strs = ["flower", "flow", "flight"]
+Output: "fl"
+
+Input: strs = ["dog", "racecar", "car"]
+Output: ""
+"""
+
+a = ["flower", "flow", "flight"]
+Solution = Solution()
+print(Solution.largest(a))
